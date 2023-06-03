b759670d605f63c9010a73bd9db099b5a8837763
[AI7/2반] 정주현 <nb3065@gmail.com>
2023-06-03 13:53:43 +0900

    Feat: AppRouter.jsx 라우터 설정완료

---
+++ client/src/components/Homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+// 담당: 정주현
+
+// import TopPhoto from "./TopPhoto";
+
+const HomePage = () => {
+    return (
+        <>
+        {/* <TopPhoto /> */}
+         <div>Homepage Component</div>
+        </>
+    )
+}
+
+export default HomePage;
---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+// 담당: 정주현
+
+import AppRouter from "../AppRouter";
+
+const Layout = () => {
+    return (
+        <>
+        <AppRouter />
+        </>
+    )
+};
+
+export default Layout;
