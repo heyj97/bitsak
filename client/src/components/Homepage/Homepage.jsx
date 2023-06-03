ad0731fa53d4a6881057a744c9885c5fd7a6e088
[AI7/2반]정주현 <nb3065@gmail.com>
2023-06-03 08:00:40 +0000

    Feature/정주현

---
+++ client/src/components/Homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+// 담당: 정주현
+
+import HomepageTopPhoto from "./HomepageTopPhoto";
+import HomepageBotMenu from "./HomepageBotMenu";
+
+const HomePage = () => {
+  return (
+    <>
+      <HomepageTopPhoto />
+      <HomepageBotMenu />
+    </>
+  );
+};
+
+export default HomePage;
