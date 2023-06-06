059c967c6dc1f80fa18bc94f83b6ec3112ba8b3c
nayeon <wkdskdus@gmail.com>
2023-06-06 11:37:57 +0900

    백엔드 GroupRank 코딩 중

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const app = express();
+const PORT = process.env.SERVER_PORT;
+
 app.use(cors());
 
 app.use(express.json());
@@ -12,6 +15,8 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.send("데이터 프로젝트 API 입니다.");
 });
+
+// require('./routes/GroupRank.routes')(app);
 
 app.listen(PORT, () => {
   console.log(`정상적으로 서버를 시작했습니다. http://localhost:${PORT}`);
