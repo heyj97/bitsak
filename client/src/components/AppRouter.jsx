0f83c8d5af7dabab47d5bf89c2fd164091561e62
[AI7/2반]정주현 <nb3065@gmail.com>
2023-06-06 03:59:46 +0000

    Feature/정주현

--- client/src/components/AppRouter.jsx
+++ client/src/components/AppRouter.jsx
@@ -1,26 +1,22 @@
 /* eslint-disable react/no-unescaped-entities */
 // 담당: 정주현
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from './HomePage/HomePage';
-import Header from './Layout/Header';
-import InforPage from './InforPage/InforPage';
-import QuizePage from './QuizePage/QuizePage';
-import MapPage from './MapPage/MapPage';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage/HomePage";
+import InforPage from "./InforPage/InforPage";
+import QuizePage from "./QuizePage/QuizePage";
+import MapPage from "./MapPage/MapPage";
 
 const AppRouter = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Header />} />
-          <Route exact element={<HomePage />} />
-          <Route path="/infor" element={<InforPage />} />
-          <Route path="/quize" element={<QuizePage />} />
-          <Route path="/map" element={<MapPage />} />
-        <Route path="*" element={<div>There's nothing here!</div>} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />
+      <Route path="/infor" element={<InforPage />} />
+      <Route path="/quize" element={<QuizePage />} />
+      <Route path="/map" element={<MapPage />} />
+      <Route path="*" element={<div>There's nothing here!</div>} />
+    </Routes>
   );
-}
+};
 
 export default AppRouter;
