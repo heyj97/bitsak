900d7e5265a138f670ce86bdb8718d4124e48358
[AI7/2반]정주현 <nb3065@gmail.com>
2023-06-06 14:21:50 +0000

    Feature/정주현

---
+++ client/src/constants/Quiz.js
@@ -0,0 +1,27 @@
+export const quizApi = [
+  {
+    question: "첫 번째 질문",
+    answer: true,
+    description: "첫 번째 질문의 해설",
+  },
+  {
+    question: "두 번째 질문",
+    answer: true,
+    description: "두 번째 질문의 해설",
+  },
+  {
+    question: "세 번째 질문",
+    answer: false,
+    description: "세 번째 질문의 해설",
+  },
+  {
+    question: "네 번째 질문",
+    answer: true,
+    description: "네 번째 질문의 해설",
+  },
+  {
+    question: "다섯 번째 질문",
+    answer: false,
+    description: "다섯 번째 질문의 해설",
+  },
+];
