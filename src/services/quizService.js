e8969b69164c17e7d0260a2fd86e7d4985d72945
nayeon <wkdskdus@gmail.com>
2023-06-09 12:57:22 +0900

    파일명 제대로 바꿨습니다. 이번엔 진짜..

---
+++ src/services/quizService.js
@@ -0,0 +1,19 @@
+import { quizModel } from '../models/quizModel.js';
+
+class quizService {
+    // 질문 5개씩 랜덤으로 불러오기
+    static async getQuestions(){
+        const questions = await quizModel.getQuestions();
+
+        //데이터가 없을 때
+        if(!questions) {
+            const errorMessage = '해당하는 데이터가 없습니다.';
+            return {errorMessage};
+        }
+
+        return questions;
+    }
+
+}
+
+export { quizService };
