aefa1895716233289b580b4f1d534a3738ecbea6
[AI7/2반]정주현 <nb3065@gmail.com>
2023-06-10 06:33:03 +0000

    fix: feature/김한빈 > develop Merge Conflict 해결

---
+++ client/src/components/Quiz/QuizHome.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const QuizHome = () => {
+  return (
+    <div>
+      <h2>Quiz 홈페이지 입니다.</h2>
+      <Link to="/quiz-home">시작하기</Link>
+    </div>
+  );
+};
+
+export default QuizHome;
