5c2392811d58095afbf1db09f808edf67a91dbc7
Heyj <nb3065@gmail.com>
2023-06-12 14:20:31 +0900

    Refactor: 2주차 코드리뷰 수정

--- client/src/components/Introduce/GlobalLightPollutionChart.jsx
+++ client/src/components/Introduce/GlobalLightPollutionChart.jsx
@@ -1,36 +1,43 @@
-import { useEffect } from "react";
-import { API_BASE_URL, API_PORT } from "../../constants/api";
 import useGetFetch from "../../hooks/useGetFetch";
-import Spinner from "../../utils/Spinner/Spinner";
+import Error from "../common/Error/Error";
+import Spinner from "../common/Spinner/Spinner";
 
 const GlobalLightPollutionChart = () => {
-    const url = `http://${API_BASE_URL}:${API_PORT}/g20-rank`;
-    const {data, isLoading, error} = useGetFetch(url)
-    if (error) return <div>Error!</div>
+  const { data, isLoading, error } = useGetFetch("g20-rank");
+  if (error)
     return (
-        <div>
-            <h2>
-                Global Light Pollution Chart
-            </h2>
-            {isLoading ? <Spinner /> : Array.isArray(data) && data.length > 0 && <DataTest resData={data} />}
-        </div>
-    )
-}
+      <div>
+        <Error error={error} />
+      </div>
+    );
+  return (
+    <div>
+      <h2>Global Light Pollution Chart</h2>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        Array.isArray(data) && data.length > 0 && <DataTest resData={data} />
+      )}
+    </div>
+  );
+};
 
-const DataTest = (resData) => {
-    return(
-        <>
-        {resData.resData && resData.resData.length > 0 && resData.resData.map((data, idx) => {
-            return(
-                <div key={idx}>
-                    <h3>Country: {data.country}</h3>
-                    <h5>Avg_mean: {data.avg_mean}</h5>
-                    <p>Avg_trend: {data.trend}</p>
-                </div>
-            )
-        })
-        }</>
-    )
-}
+const DataTest = ({ resData }) => {
+  return (
+    <>
+      {resData &&
+        resData.length > 0 &&
+        resData.map((data, idx) => {
+          return (
+            <div key={idx}>
+              <h3>Country: {data.country}</h3>
+              <h5>Avg_mean: {data.avg_mean}</h5>
+              <p>Avg_trend: {data.trend}</p>
+            </div>
+          );
+        })}
+    </>
+  );
+};
 
 export default GlobalLightPollutionChart;
--- client/src/components/Introduce/Introduce.jsx
+++ client/src/components/Introduce/Introduce.jsx
@@ -1,28 +1,33 @@
 import useGetFetch from "../../hooks/useGetFetch";
-import { API_BASE_URL, API_PORT } from "../../constants/api";
 import WordCloud from "./WordCloud";
 import styles from "./Introduce.module.css";
-import Spinner from "../../utils/Spinner/Spinner";
+import Spinner from "../common/Spinner/Spinner";
 import GlobalLightPollutionChart from "./GlobalLightPollutionChart";
+import Error from "../common/Error/Error";
 
 const Introduce = () => {
-  // const url = `http://${API_BASE_URL}:${API_PORT}/complaints`;
-  // const { data, isLoading, error } = useGetFetch(url);
+  const { data, isLoading, error } = useGetFetch("complaints");
 
-  // if (error) {
-  //   console.log("에러발생:", error);
-  // }
-
+  if (error)
+    return (
+      <div>
+        <Error />
+      </div>
+    );
   return (
     <div className={styles.wholeContainer}>
       <GlobalLightPollutionChart />
-      {/* <div className={styles.wordCloudBox}>
-      <div className={styles.wordCloudTitle}>
-        <h2>1년 동안 가장 많이 발생한 빛공해 관련 민원</h2>
-        <h4>2023.06.01 기준</h4>
+      <div className={styles.wordCloudBox}>
+        <div className={styles.wordCloudTitle}>
+          <h2>1년 동안 가장 많이 발생한 빛공해 관련 민원</h2>
+          <h4>2023.06.01 기준</h4>
+        </div>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          Array.isArray(data) && data.length > 0 && <WordCloud resData={data} />
+        )}
       </div>
-      {isLoading ? <Spinner /> : Array.isArray(data) && data.length > 0 && <WordCloud resData={data} />}
-      </div> */}
     </div>
   );
 };
