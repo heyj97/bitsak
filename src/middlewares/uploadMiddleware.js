70c8bb6d6506d04c23dd30fc2481b5017128c4a2
[AI7/2반]김희찬 <khc0023@gmail.com>
2023-06-12 22:15:34 +0900

    gallery ...ing

--- src/middlewares/uploadMiddleware.js
+++ src/middlewares/uploadMiddleware.js
@@ -12,7 +12,17 @@ const storage = multer.diskStorage({
   }
 });
 
-const uploadMiddleware = multer({ storage: storage }).single('file_path');
+const filter = (req, file, callback) => {
+  const fileType = file.mimetype.split('/')[1];
+
+  if (fileType === 'jpg' || fileType === 'jpeg' || fileType === 'png') {
+    callback(null, true);
+  } else {
+    callback({ message: '지정된 확장자만 가능합니다. (jpg, jpeg, png)' }, false);
+  }
+};
+
+const uploadMiddleware = multer({ storage: storage, fileFilter: filter }).single('file_path');
 
 
 export default uploadMiddleware;
--- src/routers/galleryRouter.js
+++ src/routers/galleryRouter.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { galleryController } from '../controllers/galleryController.js';
+import { uploadPhoto, updatePhoto, deletePhoto, getPhotosByLocation } from '../controllers/galleryController.js';
 import uploadMiddleware from '../middlewares/uploadMiddleware.js';
 
 const galleryRouter = Router();
 
-
-galleryRouter.post('/api/gallery', uploadMiddleware, galleryController);
-galleryRouter.put('/api/gallery/:galleryId', galleryController);
-galleryRouter.delete('/api/gallery/:galleryId', galleryController);
+galleryRouter.post('/api/gallery', uploadMiddleware, uploadPhoto);
+galleryRouter.put('/api/gallery/:galleryId', updatePhoto);
+galleryRouter.delete('/api/gallery/:galleryId', deletePhoto);
+galleryRouter.get('/api/gallery/:location', getPhotosByLocation);
 
 export { galleryRouter };
