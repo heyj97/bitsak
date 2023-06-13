5b6365cd0535f890d69ea45382943f96852be624
Heyj <nb3065@gmail.com>
2023-06-14 03:41:13 +0900

    Feat: 서대문구 민원위치 마커찍기 완료

--- client/src/components/Introduce/Introduce.jsx
+++ client/src/components/Introduce/Introduce.jsx
@@ -2,14 +2,16 @@ import styles from "./Introduce.module.css";
 import GlobalLightPollutionChart from "./GlobalLightPollutionChart";
 import YearlyComplaints from "./YearlyComplaints";
 import KoreaLightPollutionChart from "./KoreaLightPollutionChart";
+import ComplaintsMarkerMap from "./ComplaintsMarkerMap";
 
 const Introduce = () => {
   return (
     <>
       <div className={styles.wholeContainer}>
-        {/* <GlobalLightPollutionChart /> */}
-        {/* <KoreaLightPollutionChart /> */}
-        {/* <YearlyComplaints /> */}
+        <GlobalLightPollutionChart />
+        <KoreaLightPollutionChart />
+        <ComplaintsMarkerMap />
+        <YearlyComplaints />
       </div>
     </>
   );
