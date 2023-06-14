ab8eeb590f5cfddcfc153c64ce134b3660a72110
[AI7/2반]김희찬 <khc0023@gmail.com>
2023-06-14 12:16:23 +0900

    router 에러 해결

--- src/routers/minwonRouter.js
+++ src/routers/minwonRouter.js
@@ -6,23 +6,19 @@ dotenv.config();
 
 const minwonRouter = Router();
 
-minwonRouter.get("/api/complaints", async (req, res) => {
-    //const serviceKey = process.env.SERVICE_KEY;
-    const url = process.env.API_URL;
-  
-    
-    //let paramsURI = `${("serviceKey")}=${serviceKey}&target=pttn,dfpt,saeol&dateFrom=20220601&dateTo=20230601&resultCount=30&dataType=json&searchword=%EB%B9%9B%EA%B3%B5%ED%95%B4`;
 
+minwonRouter.get("/api/complaints", async (req, res) => {
+
+  const url = process.env.URL;
   
-    //const url = apiUrl + paramsURI;
-    try {
-        const result = await axios.get(url);
-        
-        res.json(result.data);
-    } catch (err) {
-      console.log(err);
-    }
-  });
+  try {
+      const result = await axios.get(url);
+      
+      res.json(result.data);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 
 export { minwonRouter };
