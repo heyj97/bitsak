a70d37dbed71d9b1a5398dd50522c71d06542d77
Heyj <nb3065@gmail.com>
2023-06-15 14:12:48 +0900

    Feat: 소개페이지 최종

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,3 +1,2 @@
 export const API_BASE_URL =
-  process.env.NODE_ENV === "production" ? "34.64.158.180" : "localhost";
-export const API_PORT = process.env.NODE_ENV === "production" ? 80 : 5001;
+  process.env.NODE_ENV === "production" ? "34.64.158.180" : "localhost:5001";
