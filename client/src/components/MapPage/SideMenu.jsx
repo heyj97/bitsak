70cf8dfb994056aae539ab0de2b91b2a35268aa2
Heyj <nb3065@gmail.com>
2023-06-15 15:02:56 +0900

    갤러리 작업중...

---
+++ client/src/components/MapPage/SideMenu.jsx
@@ -0,0 +1,13 @@
+import styles from "./MapPage.module.css";
+
+const SideMenu = () => {
+  return (
+    <>
+      <div className={styles.sideMenuContainer}>
+        <h2>List</h2>
+      </div>
+    </>
+  );
+};
+
+export default SideMenu;
